136aead93cd574292da280e61c7551a83819a61e
ying <annchen20092009@hotmail.com>
2022-07-25 12:33:08 +0200

    added more work

--- src/word1/Hello.java
+++ src/word1/Hello.java
@@ -3,5 +3,6 @@ package word1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Word1 started");
+        System.out.println("Created Git Repository");
     }
 }
